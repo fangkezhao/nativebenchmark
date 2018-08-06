f8e3d3568d58751551583999f2784f7fce906a78
root <root@root.com>
2018-08-06 16:25:39 +0800

    change

---
+++ native_method_overloading/src/main/jni/method_overloading.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Fengguo Wei on 6/12/17.
+//
+
+#include <android/log.h>
+#include <jni.h>
+#include <stddef.h>
+
+#define LOG_TAG    "method_overloading"
+#define LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+
+extern "C" {
+JNIEXPORT void JNICALL
+Java_org_arguslab_native_1method_1overloading_MainActivity_send__I(JNIEnv *env, jobject thisObj,
+                                                                   jint data);
+JNIEXPORT void JNICALL
+Java_org_arguslab_native_1method_1overloading_MainActivity_send___3I_3Ljava_lang_String_2Ljava_lang_String_2D(
+        JNIEnv *env, jobject thisObj, jintArray array, jobjectArray array2, jstring data,
+        jdouble d);
+}
+
+JNIEXPORT void JNICALL
+Java_org_arguslab_native_1method_1overloading_MainActivity_send__I(JNIEnv *env, jobject thisObj,
+                                                                   jint data) {
+    LOGI("%s", data); // leak
+    return;
+}
+
+JNIEXPORT void JNICALL
+Java_org_arguslab_native_1method_1overloading_MainActivity_send___3I_3Ljava_lang_String_2Ljava_lang_String_2D(
+        JNIEnv *env, jobject thisObj, jintArray array, jobjectArray array2, jstring data,
+        jdouble d) {
+    return;
+}
